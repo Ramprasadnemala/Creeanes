14bbcee3ca80a0c53693c12b253d7223d0a346cf
Ramprasadnemala <Ramprasadnemala12@gmail.com>
2024-05-31 11:44:48 +0530

    path changes

--- client/src/components/AllData/AllUserData.jsx
+++ client/src/components/AllData/AllUserData.jsx
@@ -32,7 +32,7 @@ const AllUserData = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/employees/add-emp', {
+      const response = await fetch('https://newtrick-server.vercel.app/products/add-emp', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
